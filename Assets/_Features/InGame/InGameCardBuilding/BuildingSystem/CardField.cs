ffccc0dd04be5561b87a755036e5a6840804cf60
Neeko <61304515+Neeko-onTheRoad@users.noreply.github.com>
2026-07-14 10:17:47 +0900

    A

--- Assets/_Features/InGame/InGameCardBuilding/BuildingSystem/CardField.cs
+++ Assets/_Features/InGame/InGameCardBuilding/BuildingSystem/CardField.cs
@@ -2,10 +2,21 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardField : SingletonBehaviour<CardField, SceneScope>
-{
-    [SerializeField]
+public class CardField : SingletonBehaviour<CardField, SceneScope> {
+
+	//======================================================================| Fields
+
+	[SerializeField]
     private GameObject _slotPrefab;
+	
+	[SerializeField]
+	private Transform _slotField;
+
+	[SerializeField]
+	private Transform _specialSlotField;
+
+	[SerializeField]
+	private Transform _cardField;
 
     [SerializeField]
     private Vector2 _gridSize;
@@ -14,31 +25,40 @@ public class CardField : SingletonBehaviour<CardField, SceneScope>
     private readonly HashSet<Vector2Int> _activeSlots = new();
     private readonly Dictionary<Vector2Int, GameObject> _slotInstances = new();
 
-    public event Action CardsChanged;
+	//======================================================================| Event
+
+	public event Action CardsChanged;
+
+	//======================================================================| Properties
+
+	public Transform SlotField => _slotField;
+	public Transform SpecialSlotField => _specialSlotField;
+	public Transform CardFieldTransform => _cardField;
 
     public IEnumerable<Vector2Int> ActiveSlots => _activeSlots;
     public IReadOnlyDictionary<Vector2Int, Card> PlacedCards => _placedCards;
     public IReadOnlyDictionary<Vector2Int, GameObject> SlotInstances => _slotInstances;
 
-    private void Start()
-    {
+	//======================================================================| Unity Methods
+
+	private void Start() {
         CalculateSlotPositions();
         RedrawSlots();
         CardsChanged?.Invoke();
     }
 
-    private void OnEnable()
-    {
+    private void OnEnable() {
         CardBuildingManager.OnRestarted += OnReset;
     }
 
-    private void OnDisable()
-    {
+    private void OnDisable() {
         CardBuildingManager.OnRestarted -= OnReset;
     }
 
-    public bool PlaceCard(Vector2Int position, Card card)
-    {
+	//======================================================================| Methods
+
+	public bool PlaceCard(Vector2Int position, Card card) {
+
         if (!_activeSlots.Contains(position)) return false;
 
         _placedCards[position] = card;
@@ -48,29 +68,30 @@ public class CardField : SingletonBehaviour<CardField, SceneScope>
         CardsChanged?.Invoke();
 
         return true;
+
     }
 
-    private void OnReset()
-    {
+    private void OnReset()  {
+
         _placedCards.Clear();
 
         CalculateSlotPositions();
         RedrawSlots();
         CardsChanged?.Invoke();
+
     }
 
-    private void CalculateSlotPositions()
-    {
+    private void CalculateSlotPositions() {
+
         _activeSlots.Clear();
 
-        if (_placedCards.Count == 0)
-        {
+        if (_placedCards.Count == 0) {
             _activeSlots.Add(Vector2Int.zero);
             return;
         }
 
-        foreach (KeyValuePair<Vector2Int, Card> pair in _placedCards)
-        {
+        foreach (KeyValuePair<Vector2Int, Card> pair in _placedCards) {
+
             if (pair.Value.BaseStatus.IsBlockingAttachment) continue;
 
             AddSlotIfAvailable(pair.Key + Vector2Int.up);
@@ -78,27 +99,33 @@ public class CardField : SingletonBehaviour<CardField, SceneScope>
             AddSlotIfAvailable(pair.Key + Vector2Int.left);
             AddSlotIfAvailable(pair.Key + Vector2Int.right);
         }
+
     }
 
-    private void AddSlotIfAvailable(Vector2Int position)
-    {
+    private void AddSlotIfAvailable(Vector2Int position) {
+
         if (_placedCards.ContainsKey(position)) return;
-
         _activeSlots.Add(position);
+
     }
 
-    private void RedrawSlots()
-    {
-        foreach (GameObject instance in _slotInstances.Values) Destroy(instance);
+    private void RedrawSlots() {
+
+        foreach (GameObject instance in _slotInstances.Values) {
+			Destroy(instance);
+		}
 
         _slotInstances.Clear();
 
-        foreach (Vector2Int position in _activeSlots)
-        {
-            GameObject instance = Instantiate(_slotPrefab, transform, false);
+        foreach (Vector2Int position in _activeSlots) {
+
+            GameObject instance = Instantiate(_slotPrefab, _slotField, false);
             instance.transform.localPosition = _gridSize * position;
 
             _slotInstances[position] = instance;
+
         }
+
     }
+
 }
