1556e8f01dd8acf79049cdb7b60edecaa7e2dfe1
cranberryandmixnuts <sujin83668252@gmail.com>
2026-07-15 15:42:10 +0900

    MouseInverseParallax

---
+++ Assets/_Shared/UIGeneral/SinHover.cs
@@ -0,0 +1,45 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public sealed class SinHover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    [MinValue(0f)]
+    private float amplitude = 0.5f;
+
+    [SerializeField]
+    [MinValue(0f)]
+    [SuffixLabel("회/초")]
+    private float frequency = 1f;
+
+    private Vector3 previousOffset;
+    private float elapsedTime = 0;
+
+    private void OnEnable()
+    {
+        previousOffset = Vector3.zero;
+    }
+
+    private void LateUpdate()
+    {
+        elapsedTime += Time.deltaTime;
+
+        float angle = elapsedTime * frequency * Mathf.PI * 2f;
+        Vector3 offset = Vector3.up * Mathf.Sin(angle) * amplitude;
+
+        target.localPosition = target.localPosition - previousOffset + offset;
+        previousOffset = offset;
+    }
+
+    private void OnDisable()
+    {
+        if (target == null)
+            return;
+
+        target.localPosition -= previousOffset;
+        previousOffset = Vector3.zero;
+    }
+}
