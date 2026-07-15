00b060ae85462b9c409f38f716027748528e23fa
cranberryandmixnuts <sujin83668252@gmail.com>
2026-07-15 10:00:09 +0900

    lanch fix

--- Assets/_Features/InGame/InGameCombat/Projectile/Player/CardProjectile.cs
+++ Assets/_Features/InGame/InGameCombat/Projectile/Player/CardProjectile.cs
@@ -36,7 +36,7 @@ public sealed class CardProjectile : MonoBehaviour
 
         foreach (Collider2D cardCollider in GetComponentsInChildren<Collider2D>(true)) cardCollider.isTrigger = true;
 
-        body.linearVelocity = Vector2.up * speed;
+        body.linearVelocity = Vector2.up * speed * 15;
         Destroy(gameObject, settings.ProjectileLifetime);
     }
 
