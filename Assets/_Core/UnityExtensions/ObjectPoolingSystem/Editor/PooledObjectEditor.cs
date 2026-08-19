aacaa4ee4eccdfb6d4d1d1227fe528aa1ea471cd
cranberryandmixnuts <sujin83668252@gmail.com>
2026-08-20 07:56:00 +0900

    Update PooledObjectEditor.cs

--- Assets/_Core/UnityExtensions/ObjectPoolingSystem/Editor/PooledObjectEditor.cs
+++ Assets/_Core/UnityExtensions/ObjectPoolingSystem/Editor/PooledObjectEditor.cs
@@ -1,3 +1,4 @@
+using System;
 using Sirenix.OdinInspector.Editor;
 using UnityEditor;
 using UnityEngine;
@@ -11,7 +12,49 @@ internal sealed class PooledObjectEditor : OdinEditor
         base.OnInspectorGUI();
 
         EditorGUILayout.Space();
+        DrawSingletonConflictValidation();
         DrawRootValidation();
+    }
+
+    private void DrawSingletonConflictValidation()
+    {
+        for (int i = 0; i < targets.Length; i++)
+        {
+            var pooledObject = (PooledObject)targets[i];
+            if (pooledObject == null)
+                continue;
+
+            MonoBehaviour[] behaviours =
+                pooledObject.GetComponentsInChildren<MonoBehaviour>(true);
+
+            for (int j = 0; j < behaviours.Length; j++)
+            {
+                MonoBehaviour behaviour = behaviours[j];
+                if (behaviour == null || !InheritsSingletonBehaviour(behaviour.GetType()))
+                    continue;
+
+                EditorGUILayout.HelpBox(
+                    $"'{pooledObject.name}' cannot be safely pooled because " +
+                    $"'{behaviour.GetType().Name}' on '{behaviour.name}' inherits " +
+                    "SingletonBehaviour. Pooling creates multiple instances, which " +
+                    "conflicts with singleton lifetime rules.",
+                    MessageType.Error);
+            }
+        }
+    }
+
+    private static bool InheritsSingletonBehaviour(Type type)
+    {
+        for (Type current = type; current != null; current = current.BaseType)
+        {
+            if (current.IsGenericType &&
+                current.GetGenericTypeDefinition() == typeof(SingletonBehaviour<,>))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void DrawRootValidation()
